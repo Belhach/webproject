1ae1a6c7194f5e3f794415a73de510ea56b947b2
mhamedbelhachemi <mhamed79@live.fr>
2020-03-28 02:58:33 +0100

    affichage du logo

---
+++ client/src/components/ChatWindow.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import logo from '../images/logo.png';
+
+
+const ChatWindow = () => {
+    return (
+        <img src={logo} />
+    )
+}
+
+export default ChatWindow;
---
+++ client/src/components/JoinWindow.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import logo from '../images/logo.png';
+
+const JoinWindow = () => {
+    return (
+        <img src={logo} />
+    )
+}
+
+export default JoinWindow;
